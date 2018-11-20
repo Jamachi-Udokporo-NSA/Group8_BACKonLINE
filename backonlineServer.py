2cd57395bc7df182b7d754b82657232d301fcb2f
Daniel Addis <AddisD@cardiff.ac.uk>
2018-11-20 14:00:16 +0000

    Create server template model

--- backonlineServer.py
+++ backonlineServer.py
@@ -1,26 +1,15 @@
 import os
-from flask import Flask, redirect, request, render_template, make_response, escape, session
+from flask import Flask, redirect, request, render_template, make_response, escape, session, jsonify
 import sqlite3
-
-DATABASE = 'Database/backonlineDatabase.db'
 
 app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-@app.route("/Admin")
-def admin():
-    username = request.cookies.get('username')
-    return render_template('Admin.html', msg = '', username = username)
-
-# =======================================================================
-#      the db creation methods
-def deleteTables():
-    conn = sqlite3.connect(DATABASE)
-    conn.execute('DROP TABLE IF EXISTS Jobs')
-    conn.execute('DROP TABLE IF EXISTS Orders')
-    conn.execute('DROP TABLE IF EXISTS Customers')
-    conn.close()
+@app.route("/Main", methods=['GET'])
+def returnFirst():
+    if request.method == 'GET':
+        return render_template('Template.html')
 
 if __name__ == "__main__":
-    pass
+    app.run(debug=True)
