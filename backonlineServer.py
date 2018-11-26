11533aae78c82b0b0dc8de6702cb9f2d649a829b
Daniel Addis <AddisD@cardiff.ac.uk>
2018-11-26 11:24:40 +0000

    Retrive data from a data

--- backonlineServer.py
+++ backonlineServer.py
@@ -4,21 +4,32 @@ import sqlite3
 
 app = Flask(__name__)
 
-DATABASE = 'backonlinedatabase.db'
+DATABASE = 'Database/backonlinedatabase.db'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @app.route("/Main", methods=['GET'])
 def returnFirst():
-        if request.method == 'GET':
-                return render_template('Template.html')
+    if request.method == 'GET':
+        return render_template('Template.html')
 
+@app.route("/Getdata")
 def fetchingdata():
+    try:
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
-        cur.execute('select* from Question, where QuestionID = "1"' )
+        cur.execute("SELECT * FROM Question;")
         data = cur.fetchall()
-       
-                
+        print(data)
+        return render_template('Template.html', data = data)
+
+    except:
+        print('there was an error')
+        return str(data)
+
+    finally:
+        conn.close()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
