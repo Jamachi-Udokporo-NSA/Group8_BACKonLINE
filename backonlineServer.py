62e5e3d37aa4d3f53cc5c78ec38c77674920f19f
Udokporo Jamachi <UdokporoJB@cardiff.ac.uk>
2018-11-29 16:00:57 +0000

    Added the Welcome template

--- backonlineServer.py
+++ backonlineServer.py
@@ -91,5 +91,11 @@ def getSurveyD():
 
     except:
         return 'there was an error'
+
+@app.route("/Welcome", methods = ['GET'])
+def returnwel():
+    if request.method == 'GET':
+        return render_template('WelcomePage.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
