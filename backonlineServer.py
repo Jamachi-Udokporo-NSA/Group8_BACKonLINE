76e1fa252a153cf05981942c16fc187ead184403
Daniel Addis <AddisD@cardiff.ac.uk>
2018-11-30 13:13:55 +0000

    Survey question layout overhall

--- backonlineServer.py
+++ backonlineServer.py
@@ -6,90 +6,39 @@ app = Flask(__name__)
 
 DATABASE = 'Database/backonlinedatabase.db'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
+QuestGroup = 0
 @app.route("/Main", methods=['GET'])
 def returnFirst():
     if request.method == 'GET':
         return render_template('Template.html')
 
-@app.route("/SurveyA")
-def getSurveyA():
+@app.route("/Survey")
+def getSurvey():
+    global QuestGroup
+
     try:
+        QuestGroup += 1
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM Question WHERE QuestionID BETWEEN 1 AND 22;")
+        cur.execute(f"SELECT * FROM Question WHERE QuestionGroup == {QuestGroup};")
         questionData = cur.fetchall()
         conn.close()
 
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
-        cur.execute("SELECT * FROM Answer WHERE QuestionID BETWEEN 1 AND 22; ")
+        cur.execute("SELECT * FROM Answer;")
         answerData = cur.fetchall()
         conn.close()
 
-        return render_template('SurveyAPainBehaviour.html',questionData = questionData, answerData= answerData)
+        if(QuestGroup<15):
+            return render_template('Survey.html',questionData = questionData, answerData= answerData)
+
+        else:
+            QuestGroup -= 1
+            return render_template('SurveyEnd.html',questionData = questionData, answerData= answerData)
 
     except:
         return 'there was an error'
 
-@app.route("/SurveyB")
-def getSurveyB():
-    try:
-        conn = sqlite3.connect(DATABASE)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Question WHERE QuestionID BETWEEN 23 AND 28; ")
-        questionData = cur.fetchall()
-        conn.close()
-
-        conn = sqlite3.connect(DATABASE)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Answer WHERE QuestionID BETWEEN 23 AND 28; ")
-        answerData = cur.fetchall()
-        conn.close()
-
-        return render_template('SurveyBBackPainAndWork.html',questionData = questionData, answerData= answerData)
-
-    except:
-        return 'there was an error'
-
-@app.route("/SurveyC")
-def getSurveyC():
-    try:
-        conn = sqlite3.connect(DATABASE)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Question WHERE QuestionID BETWEEN 29 AND 32;")
-        questionData = cur.fetchall()
-        conn.close()
-
-        conn = sqlite3.connect(DATABASE)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Answer WHERE QuestionID BETWEEN 29 AND 32; ")
-        answerData = cur.fetchall()
-        conn.close()
-
-        return render_template('SurveyCBackPainAndLifestyle.html',questionData = questionData, answerData= answerData)
-
-    except:
-        return 'there was an error'
-
-@app.route("/SurveyD")
-def getSurveyD():
-    try:
-        conn = sqlite3.connect(DATABASE)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Question WHERE QuestionID BETWEEN 33 AND 39; ")
-        questionData = cur.fetchall()
-        conn.close()
-
-        conn = sqlite3.connect(DATABASE)
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM Answer WHERE QuestionID BETWEEN 33 AND 39; ")
-        answerData = cur.fetchall()
-        conn.close()
-
-        return render_template('SurveyDPerceptionOfBackPain.html',questionData = questionData, answerData= answerData)
-
-    except:
-        return 'there was an error'
 if __name__ == "__main__":
     app.run(debug=True)
