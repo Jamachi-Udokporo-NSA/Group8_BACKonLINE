b9c4ffe25a8280db8919d406e1a82da8d6300f3c
Daniel Addis <AddisD@cardiff.ac.uk>
2018-12-02 17:14:01 +0000

    Update server

--- backonlineServer.py
+++ backonlineServer.py
@@ -3,21 +3,19 @@ from flask import Flask, redirect, request, render_template, make_response, esca
 import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'Database/backonlinedatabase.db'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+QuestGroup = 0
 
-@app.route("/login", methods=['GET'])
+@app.route("/", methods=['GET'])
 def getlogin():
     if request.method== 'GET':
         return render_template('registration.html')
 
-
-DATABASE = 'Database/backonlinedatabase.db'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-QuestGroup = 0
-# @app.route("/Main", methods=['GET'])
-# def returnFirst():
-#     if request.method == 'GET':
-#         return render_template('Template.html')
-
+@app.route("/Welcome", methods=['GET'])
+def getWelcome():
+    if request.method== 'GET':
+        return render_template('WelcomePage.html')
 
 @app.route("/Survey")
 def getSurvey():
