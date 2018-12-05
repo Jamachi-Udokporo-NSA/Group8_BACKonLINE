a75c7877ef9df5309eb5966bbe0da1a3c4ddeae5
Daniel Addis <AddisD@cardiff.ac.uk>
2018-12-05 15:21:35 +0000

    Work on question types

--- backonlineServer.py
+++ backonlineServer.py
@@ -7,35 +7,20 @@ DATABASE = 'Database/backonlinedatabase.db'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 QuestGroup = 0
 
-
-
-@app.route("/Form", methods=['GET', 'POST'])
-def getpatient():
-    if request.method == 'GET':
+@app.route("/", methods=['GET'])
+def getlogin():
+    if request.method== 'GET':
         return render_template('registration.html')
-    if request.method == 'POST':
-        FirstName = request.form.get('FirstName', default="Error")
-        SurName = request.form.get('SurName', default="Error")
-        Email = request.form.get('Email', default="Error")
-        Password = request.form.get('Password', default="Error")
-        Age = request.form.get('Age', default="Error")
-        Gender= request.form.get('Gender', default="Error")
-        print("inserting "+FirstName)
-        try:
-            conn = sqlite3.connect(DATABASE)
-            cur = conn.cursor()
-            cur.execute("INSERT INTO Patient ('FirstName','SurName','Email','Password','Age','Gender')\
-                        VALUES (?,?,?,?,?,?)",(FirstName,SurName,Email,Password,Age,Gender))
-            conn.commit()
-            msg = "Record successfully added"
-        except:
-            conn.rollback()
-            msg = "error in insert operation"
-        finally:
-            conn.close()
-            return msg
 
-    
+#@app.route("/Patient",
+#def getpatient():
+#    conn = sqlite3.connect(DATABASE)
+#    cur = conn.cursor()
+#    cur.execute("INSERT INTO;")
+#    answerData = cur.fetchall()
+#    conn.close()
+
+
 @app.route("/Welcome", methods=['GET'])
 def getWelcome():
     if request.method== 'GET':
@@ -66,7 +51,7 @@ def getSurvey(NumT):
             return render_template('Survey.html',questionData = questionData, answerData= answerData, questionNumber = QuestGroup)
 
         else:
-            return render_template('SurveyEnd.html',questionData = questionData, answerData= answerData)
+            return render_template('SurveyEnd.html',questionData = questionData, answerData= answerData, questionNumber = QuestGroup)
 
     except:
         return 'there was an error'
@@ -76,6 +61,7 @@ def getSurveyB(NumT):
     try:
         QuestGroup = int(NumT)
         QuestGroup -=1
+
         conn = sqlite3.connect(DATABASE)
         cur = conn.cursor()
         cur.execute(f"SELECT * FROM Question WHERE QuestionGroup == {QuestGroup};")
@@ -92,7 +78,7 @@ def getSurveyB(NumT):
             return render_template('Survey.html',questionData = questionData, answerData= answerData, questionNumber = QuestGroup)
 
         else:
-            return render_template('SurveyEnd.html',questionData = questionData, answerData= answerData)
+            return render_template('SurveyEnd.html',questionData = questionData, answerData= answerData, questionNumber = QuestGroup)
 
     except:
         return 'there was an error'
